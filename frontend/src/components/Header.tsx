2ad5305c9feaf738cbc802d5017c93ae04de1196
1998KB <klajdibeqiraj88@gmail.com>
2023-03-27 18:37:25 +0200

    first real commit frontend

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import "../stylesheets/Header.css"
-import {useNavigate} from "react-router-dom";
-
+import {Link, useNavigate} from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
-            <p>Header</p>
+        <div className='header'>
+            <img className='header__logo-img' src={require('../components/Images/LogoImg.png')}/>
+            <img className='header__logo-name' src={require('../components/Images/LogoName.png')}/>
+            <input className='header__search-bar' placeholder='Search for anything'/>
+            <div className="header__navigation">
+                <Link to='/'>Home</Link>
+                <Link to='/generate'>Generate</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/gallery'>Gallery</Link>
+                <Link to='/login'>Login</Link>
+            </div>
         </div>
     );
 };
