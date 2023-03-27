6bfd9fa262068c162266c5cb7ebe62ef7328f855
1998KB <klajdibeqiraj88@gmail.com>
2023-03-27 17:18:22 +0200

    try3

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <div>
             <p>Header</p>
-            <button onClick={() => nav("/home")}>fuck off NOW</button>
+            <button onClick={() => nav("/home")}>fuck off NOW please</button>
         </div>
     );
 };
