867627e1d6765a1cf365f1e1dbb46080fa2f632d
1998KB <klajdibeqiraj88@gmail.com>
2023-03-27 16:31:14 +0200

    try

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import "../stylesheets/Header.css"
+import {useNavigate} from "react-router-dom";
 
 
 const Header = () => {
+    let nav = useNavigate();
     return (
         <div>
-<p>Header</p>
+            <p>Header</p>
+            <button onClick={() => nav("/home")}>fuck off</button>
         </div>
     );
 };
