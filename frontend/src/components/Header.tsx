1a653f3de05586be2e17d23b158a98d06bdf6f48
Joel Verkaik <joel.verkaik@appliedtechnology.se>
2023-03-27 23:48:36 +0200

    added header styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "../stylesheets/Header.css"
 import {Link, useNavigate} from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className='header'>
-            <img className='header__logo-img' src={require('../components/Images/LogoImg.png')}/>
-            <img className='header__logo-name' src={require('../components/Images/LogoName.png')}/>
-            <input className='header__search-bar' placeholder='Search for anything'/>
-            <div className="header__navigation">
-                <Link to='/'>Home</Link>
-                <Link to='/generate'>Generate</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/gallery'>Gallery</Link>
-                <Link to='/login'>Login</Link>
-            </div>
+        <div>
+            <header className='header'>
+                <img className='header__logo-img' src={require('../components/Images/LogoImg.png')}/>
+                <img className='header__logo-name' src={require('../components/Images/LogoName.png')}/>
+                <input className='header__search-bar' placeholder='Search for anything'/>
+                <div className="header__navigation">
+                    <Link to='/'>Home</Link>
+                    <Link to='/generate'>Generate</Link>
+                    <Link to='/about'>About</Link>
+                    <Link to='/gallery'>Gallery</Link>
+                    <Link to='/login'>Login</Link>
+                </div>
+            </header>
         </div>
     );
 };
