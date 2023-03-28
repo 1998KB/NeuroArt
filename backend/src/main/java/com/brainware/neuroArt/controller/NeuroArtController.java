41ce67e856dae8e8a5d8495f908319ca383e1977
1998KB <klajdibeqiraj88@gmail.com>
2023-03-28 16:18:09 +0200

    fix origin

--- backend/src/main/java/com/brainware/neuroArt/controller/NeuroArtController.java
+++ backend/src/main/java/com/brainware/neuroArt/controller/NeuroArtController.java
@@ -5,10 +5,7 @@ import com.brainware.neuroArt.service.NeuroArtService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.concurrent.ExecutionException;
@@ -26,6 +23,7 @@ public class NeuroArtController {
     }
 
     @PostMapping("/generate")
+    @CrossOrigin("https://blue-sky-0e47a0403.2.azurestaticapps.net/generate")
     public ResponseEntity<String> generateImage(@RequestBody String prompt){
         OpenApiImageDTO openApiImageDTO;
         try {
