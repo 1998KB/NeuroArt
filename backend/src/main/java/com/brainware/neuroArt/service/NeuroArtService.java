28b67dc90f768bf58c580c647707d1b5c176e789
Lukasz Krawiec <lukasz.krawiec@appliedtechnology.se>
2023-03-28 13:10:51 +0200

    Refactored generated image endpoint

--- backend/src/main/java/com/brainware/neuroArt/service/NeuroArtService.java
+++ backend/src/main/java/com/brainware/neuroArt/service/NeuroArtService.java
@@ -1,28 +1,19 @@
 package com.brainware.neuroArt.service;
 
 import com.brainware.neuroArt.model.Client;
-import com.brainware.neuroArt.model.Image;
-import com.brainware.neuroArt.model.ImageDTO;
-import com.brainware.neuroArt.model.UrlData;
+import com.brainware.neuroArt.model.OpenApiImageDTO;
 import com.brainware.neuroArt.model.repository.ClientRepository;
 import com.brainware.neuroArt.model.repository.CollectionRepository;
 import com.brainware.neuroArt.model.repository.ImageRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import org.apache.catalina.mapper.Mapper;
-import org.springframework.boot.json.GsonJsonParser;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -39,35 +30,35 @@ public class NeuroArtService {
         return clientRepository.findById(1L).orElse(null);
     }
 
-    public String getImage(String prompt) throws ExecutionException, InterruptedException {
+    public OpenApiImageDTO getImage(String prompt) throws ExecutionException, InterruptedException {
+        if (prompt.isEmpty() || prompt.isBlank()) {
+            throw new IllegalArgumentException("Prompt has to be valid");
+        }
         HttpClient client = HttpClient.newHttpClient();
         String apiKey = System.getenv("OPENAI_KEY");
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.openai.com/v1/images/generations"))
                 .header("Authorization", "Bearer " + apiKey)
                 .header("Content-Type", "application/json")
-                .method("POST", HttpRequest.BodyPublishers.ofString(prompt))
+                .method("POST", HttpRequest.BodyPublishers.ofString(String.format("""
+                        {
+                            "prompt": "%s"
+                        }
+                        """,prompt)))
                 .build();
 
         CompletableFuture<HttpResponse<String>> responseFuture = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
         return responseFuture.thenApply(response -> {
             int statusCode = response.statusCode();
-            HttpHeaders headers = response.headers();
-            String responseBody = response.body();
-            System.out.println("Status Code: " + statusCode);
-            System.out.println("Headers: " + headers);
-            System.out.println("Response Body: " + responseBody);
-
-            try {
-                ImageDTO responseObj = mapper.readValue(responseBody, ImageDTO.class);
-                System.out.println("Created: " + responseObj.created());
-                String data = responseObj.data()[0].url();
-                System.out.println("url= " + data);
-                return data;
-            } catch (IOException e) {
-                System.out.println("Error deserializing response body: " + e.getMessage());
+            if (statusCode != 200) {
+                throw new IllegalArgumentException("OpenAPI is not available right now");
             }
-            return null;
+            String responseBody = response.body();
+            try {
+                return mapper.readValue(responseBody, OpenApiImageDTO.class);
+            } catch (IOException e) {
+                throw new IllegalArgumentException("Something went wrong with fetching");
+            }
         }).get();
     }
 }
