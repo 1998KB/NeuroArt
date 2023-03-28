fd225937283a921b5c337bc8197a011c318ce081
1998KB <klajdibeqiraj88@gmail.com>
2023-03-28 14:35:18 +0200

    Dropbox component and structure

--- frontend/src/components/Generate.tsx
+++ frontend/src/components/Generate.tsx
@@ -6,7 +6,8 @@ import ImageContainer from "./ImageContainer";
 const Generate = () => {
     return (
         <div>
-            <p>Generate</p>
+            <h1 className='heading'>Be Creative</h1>
+            <p className="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet animi assumenda dicta iusto nisi officiis placeat praesentium quasi sint. Corporis deleniti illo, inventore minima molestiae mollitia nemo quia sequi voluptatibus voluptatum! Aliquid commodi, distinctio fugiat itaque magni maiores minus molestiae neque nostrum officia perferendis possimus quae quam rem veniam. Amet aperiam atque beatae consectetur, cumque deleniti deserunt dignissimos dolorem dolores doloribus eligendi enim, eos est ex in ipsam ipsum laboriosam laborum minima mollitia nostrum odit placeat quas quibusdam quos tempora tempore vel velit voluptates voluptatum. At consequuntur dolor expedita explicabo hic itaque, iusto magnam neque, obcaecati saepe sunt tempora?</p>
             <Form/>
             <ImageContainer/>
         </div>
