1b3f24e2bf44d5e87edf68f78f4baffadc0dea0c
1998KB <klajdibeqiraj88@gmail.com>
2023-03-30 12:34:36 +0200

    save button

--- frontend/src/interfaces.ts
+++ frontend/src/interfaces.ts
@@ -6,10 +6,26 @@ export interface Option {
 export interface DropdownProps {
     setPrompt: Function
     isLoading: boolean
-    setGenerated: Function
+    setGenerated: Function,
+    setIsDisabled: Function
 }
 
 export type CarouselType = {
     imageSrc: string,
     imageAlt: string
+}
+
+export interface ImageContainerProps {
+    image: string
+    isLoading: boolean
+    prompt: string,
+    isDisabled: boolean,
+    setIsDisabled: Function
+
+}
+export interface ImageSaveData {
+    temporaryUrl: string,
+    prompt:string,
+    title: string,
+    description: string
 }
