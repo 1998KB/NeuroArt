dcd8945773d39fccdae59abe70a6cf4a2b40876c
JorgeMartinez <jorge.martinez@appliedtechnology.se>
2023-04-02 17:36:33 +0200

    working on footer

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className='footer__logo'>
+                <img className='home__image-wheel' src={require('../../Images/LogoImgWhite.png')}/>
+            </div>
+            <div className='footer__contact'>
+                <h1>Contact</h1>
+                <p>✆ +31 0000000</p>
+                <p>John M. Keynesplein 12-46, 1066 EP Amsterdam</p>
+                <p>brainware@appliedtechnology.se</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
