7774119e0fb27eaf0570c5a8525328899e482cf3
LukaszKSalt <118811264+LukaszKSalt@users.noreply.github.com>
2023-04-02 13:36:23 +0200

    Lukasz (#48)
    * Google login frontend
    
    * Security backend structure
    
    * tests
    
    * added sub fetch
    
    * Working generate/user
    
    * Working backend with generate and user

--- backend/src/main/java/com/brainware/neuroArt/model/repository/ClientRepository.java
+++ backend/src/main/java/com/brainware/neuroArt/model/repository/ClientRepository.java
@@ -1,9 +1,10 @@
 package com.brainware.neuroArt.model.repository;
 
 import com.brainware.neuroArt.model.Client;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository extends CrudRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Client findClientBySub(String sub);
 }
