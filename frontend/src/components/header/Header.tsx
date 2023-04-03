d75307ec76dfd4d511944e52941daf7a885733ed
Jorge-Martinez82 <118907308+Jorge-Martinez82@users.noreply.github.com>
2023-04-03 10:43:55 +0200

    Jorge (#69)
    * login css

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import "./Header.css"
 import {Link} from "react-router-dom";
+import {User} from "../../interfaces";
 
-const Header = () => {
+interface headerProps {
+    user: User
+}
+
+const Header = (props: headerProps) => {
     return (
         <div className='header'>
             <header className='header__header'>
@@ -21,9 +26,11 @@ const Header = () => {
                     <Link to='/'>Home</Link>
                     <Link to='/generate'>Generate</Link>
                     <Link to='/about'>About</Link>
-                    <div className='gallery1'><Link to='/gallery'>Gallery</Link></div>
-                    <div className='gallery2'><Link to='/gallery2'>Gallery</Link></div>
-                    <Link to='/login'>Login</Link>
+                    <Link to='/gallery'><div className='gallery1'>Gallery</div></Link>
+                    <Link to='/gallery2'><div className='gallery2'>Gallery</div></Link>
+                    {props.user.username !== '' ? <Link to='/login'>{props.user.username}</Link>
+                        : <Link to='/login'>Login</Link>}
+
 
                 </div>
             </header>
