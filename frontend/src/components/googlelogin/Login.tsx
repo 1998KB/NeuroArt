424268f16905859bfccc23cb4863d65d5df73a4e
JorgeMartinez <jorge.martinez@appliedtechnology.se>
2023-04-03 16:05:10 +0200

    css login

--- frontend/src/components/googlelogin/Login.tsx
+++ frontend/src/components/googlelogin/Login.tsx
@@ -108,7 +108,9 @@ const Login = (props: loginProps) => {
                         console.log('Login Failed:');
                     }}
                 />}
+
             <button className='login__button__logout' onClick={handleLogout}>Logout</button>
+
         </div>
     );
 };
