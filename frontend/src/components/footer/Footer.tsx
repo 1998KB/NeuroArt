78a5aa0c3f9a065600a79d9773682a664aa51c18
Joel Verkaik <joel.verkaik@appliedtechnology.se>
2023-04-03 16:05:52 +0200

    footer + additional styling

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Footer.css"
 
 const Footer = () => {
+    const [showFooter, setShowFooter] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.pageYOffset > 70) {
+                setShowFooter(true);
+            } else {
+                setShowFooter(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    })
+
     return (
-        <div className='footer'>
-            <div className='footer__logo'>
-                <img className='footer__image-wheel' src={require('../../Images/LogoImgWhite.png')}/>
-            </div>
-            <div className='footer__contact'>
-                <h1>Contact</h1>
-                <p>✆ +31 0000000</p>
-                <p>John M. Keynesplein 12-46, 1066 EP Amsterdam</p>
-                <p>brainware@appliedtechnology.se</p>
+        <div className={`footer__container${showFooter ? " show" : ""}`}>
+            <div className='footer'>
+                    <div className='footer__logo'>
+                        <img className='footer__image-wheel' src={require('../../Images/LogoImgWhite.png')}/>
+                    </div>
+                    <div className='footer__contact'>
+                        <h1>Contact</h1>
+                        <p>✆ +31 0000000</p>
+                        <p>John M. Keynesplein 12-46, 1066 EP Amsterdam</p>
+                        <p>brainware@appliedtechnology.se</p>
+                    </div>
             </div>
         </div>
     );
