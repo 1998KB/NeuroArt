95a615430f7c5d9c4c0ec3ec1b2ba1eca4593920
LukaszKSalt <118811264+LukaszKSalt@users.noreply.github.com>
2023-04-04 11:11:58 +0200

    Worked on generate title to be mandatory

--- frontend/src/components/imageContainer/ImageContainer.tsx
+++ frontend/src/components/imageContainer/ImageContainer.tsx
@@ -5,83 +5,100 @@ import {ImageContainerProps, ImageSaveData} from "../../interfaces";
 
 const ImageContainer = (props: ImageContainerProps) => {
     const [isSaving, setIsSaving] = useState(false);
+    const [inputTitleError, setInputTitleError] = useState(false);
 
     const handleSave = async () => {
-        setIsSaving(true)
-        if (props.credentials == null) {
+        if (props.inputTitle == null) {
+            setInputTitleError(true)
             return;
         }
+        const trimmedTitle = props.inputTitle.trim();
+        if (trimmedTitle === '') {
+            setInputTitleError(true);
+            return;
+        }
+
+        setIsSaving(true);
+
         const save: ImageSaveData = {
             temporaryUrl: props.image,
             prompt: props.prompt,
-            title: props.inputTitle,
-            description: props.inputDescription
-        }
-        const response = await fetch(
-            "https://neuroart.azurewebsites.net/image",
-            {
-                method: 'POST',
+            title: trimmedTitle,
+            description: props.inputDescription,
+        };
 
-                headers: {'content-type': 'application/json',
-                    'Authorization': `Bearer ${props.credentials.credential}`},
-                body: JSON.stringify(save),
-            }
-        )
+        const response = await fetch("https://neuroart.azurewebsites.net/image", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                Authorization: `Bearer ${props.credentials?.credential}`,
+            },
+            body: JSON.stringify(save),
+        });
+
         if (!response.ok) {
             throw new Error(`Failed to save: ${response.status}`);
         }
+
         props.setIsSaved(true);
-        setIsSaving(false)
-        props.setSaveButtonText('Saved');
+        setIsSaving(false);
+        props.setSaveButtonText("Saved");
         props.setIsDisabled(false);
-    }
+    };
+
 
     return (
-        <div className='imagecontainer'>
-            {props.isLoading && <LoadingSpinner/>}
-            {props.image &&
+        <div className="imagecontainer">
+            {props.isLoading && <LoadingSpinner />}
+            {props.image && (
                 <>
                     <div className="imagecontainer__generated-image-div">
-                        <img className='imagecontainer__generated-image' src={props.image}/>
+                        <img className="imagecontainer__generated-image" src={props.image} />
                     </div>
                     <div className="imagecontainer__input-div">
                         <label className="add-title-label">Add Title: </label>
-                        <input type="text"
-                               className="imagecontainer__title-input"
-                               placeholder='Add a title'
-                               value={props.inputTitle}
-                               onChange={(e) => props.setInputTitle(e.target.value)}
+                        <input
+                            type="text"
+                            className={`imagecontainer__title-input${inputTitleError ? '-error' : ''}`}
+                            placeholder={inputTitleError ? 'Please provide title' : 'Add a title'}
+                            value={props.inputTitle}
+                            onChange={(e) => {
+                                props.setInputTitle(e.target.value);
+                                setInputTitleError(false);
+                            }}
                         />
-                        <label htmlFor="" className="add-description-label">Add Description: </label>
-                        <input type="text"
-                               className="imagecontainer__description-input"
-                               placeholder='Add a description'
-                               value={props.inputDescription}
-                               onChange={(e) => props.setInputDescription(e.target.value)}
+                        <label htmlFor="" className="add-description-label">
+                            Add Description:{" "}
+                        </label>
+                        <input
+                            type="text"
+                            className="imagecontainer__description-input"
+                            placeholder="Add a description"
+                            value={props.inputDescription}
+                            onChange={(e) => props.setInputDescription(e.target.value)}
                         />
-                        {isSaving && <div className="letter-holder">
-                            <div className="l-1 letter">S</div>
-                            <div className="l-2 letter">a</div>
-                            <div className="l-3 letter">v</div>
-                            <div className="l-4 letter">i</div>
-                            <div className="l-5 letter">n</div>
-                            <div className="l-6 letter">g</div>
-                            <div className="l-7 letter">.</div>
-                            <div className="l-8 letter">.</div>
-                            <div className="l-9 letter">.</div>
-                        </div>}
+                        {isSaving && (
+                            <div className="letter-holder">
+                                <div className="l-1 letter">S</div>
+                                <div className="l-2 letter">a</div>
+                                <div className="l-3 letter">v</div>
+                                <div className="l-4 letter">i</div>
+                                <div className="l-5 letter">n</div>
+                                <div className="l-6 letter">g</div>
+                                <div className="l-7 letter">.</div>
+                                <div className="l-8 letter">.</div>
+                                <div className="l-9 letter">.</div>
+                            </div>
+                        )}
                         <button
-                            className={
-                                props.isSaved ? "imagecontainer__save-btn saved" : "imagecontainer__save-btn"
-                            }
+                            className={props.isSaved ? "imagecontainer__save-btn saved" : "imagecontainer__save-btn"}
                             onClick={handleSave}
                             disabled={props.isDisabled}
                         >
                             {props.saveButtonText}
                         </button>
-
                     </div>
-                </>
+                </>)
             }
         </div>
     );
