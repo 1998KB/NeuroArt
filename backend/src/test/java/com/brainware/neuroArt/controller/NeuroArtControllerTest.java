2259f5e9f8d0e5d5a43a04c60d5ea764eb9dcdb3
Lukasz Krawiec <lukasz.krawiec@appliedtechnology.se>
2023-04-04 22:59:16 +0200

    before mockito

--- backend/src/test/java/com/brainware/neuroArt/controller/NeuroArtControllerTest.java
+++ backend/src/test/java/com/brainware/neuroArt/controller/NeuroArtControllerTest.java
@@ -1,5 +1,6 @@
 package com.brainware.neuroArt.controller;
 
+import com.brainware.neuroArt.model.Client;
 import com.brainware.neuroArt.model.repository.ClientRepository;
 import com.brainware.neuroArt.model.repository.CollectionRepository;
 import com.brainware.neuroArt.model.repository.ImageRepository;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -85,5 +87,35 @@ public class NeuroArtControllerTest {
                                 .claim("sub", "123"))))
                 .andExpect(status().isNoContent())
                 .andExpect(content().string(""));
+    }
+
+    @Test
+    void shouldReturn400WhenGetImageNotExisting() throws Exception {
+        mockMvc.perform(get("/image/1"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("Image with given id not found!"));
+    }
+
+//    @Test
+    void shouldGenerateImage() throws Exception {
+        // given
+        createClient();
+
+        mockMvc.perform(post("/generate")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("aswdw")
+                .with(SecurityMockMvcRequestPostProcessors.jwt().jwt(jwt -> jwt
+                        .claim("sub", "123"))))
+                .andExpect(status().isCreated())
+                .andExpect(content().string(""));
+    }
+
+    private void createClient() throws Exception {
+        mockMvc.perform(post("/user")
+                .with(SecurityMockMvcRequestPostProcessors.jwt().jwt(jwt -> jwt
+                        .claim("sub", "123")
+                        .claim("given_name", "Lukas")
+                        .claim("email", "email@email.com")
+                        .claim("picture", "url://picture"))));
     }
 }
