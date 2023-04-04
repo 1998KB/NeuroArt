da026348fb0b4854a0a063b71521e5f3f3531cb2
Lukasz Krawiec <lukasz.krawiec@appliedtechnology.se>
2023-04-04 22:17:02 +0200

    First test

--- backend/src/test/java/com/brainware/neuroArt/controller/NeuroArtControllerTest.java
+++ backend/src/test/java/com/brainware/neuroArt/controller/NeuroArtControllerTest.java
@@ -1,19 +1,15 @@
 package com.brainware.neuroArt.controller;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -21,4 +17,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class NeuroArtControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldThrowUnauthorizedOnGenerate() throws Exception {
+        mockMvc.perform(post("/generate"))
+                .andExpect(status().isUnauthorized());
+    }
 }
